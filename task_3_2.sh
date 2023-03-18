add505fb402f632b6a0de51a3bc02295cbea952f
Denis Novoselov <d.novoselov@arenadata.io>
2023-03-18 21:00:57 +0500

    first task

---
+++ task_3_2.sh
@@ -0,0 +1,6 @@
+# Script that searchs files with .txt extension in folder
+# script igores sub folders
+# All founded files are copy or move to user home folder
+# User can chose copy or move files
+# By default, files are copy
+
